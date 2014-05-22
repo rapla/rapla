aca0cf3a7e10d63cb01998eacb153160e56b0b7f
Bob Jordaens <icare4.rapla@gmail.com>
2014-05-22 21:37:12 +0200

    Cosmetic changes in html vieuws

--- src/org/rapla/components/calendarview/html/HTMLCompactWeekView.java
+++ src/org/rapla/components/calendarview/html/HTMLCompactWeekView.java
@@ -102,9 +102,9 @@ public class HTMLCompactWeekView extends AbstractHTMLView {
         StringBuffer result = new StringBuffer();
         result.append("<table class=\"month_table\">\n");
         result.append("<tr>\n");
-        result.append("<td class=\"week_number\" width=\"" + Math.round(getLeftColumnSize() * 100) + "%\">");
+        result.append("<th class=\"week_number\">");
         result.append(weeknumber);
-        result.append("</td>");
+        result.append("</th>");
         String percentage = "" + Math.round(95.0 / (Math.max(0, columns)));
          
         int rowsize = rows.size();
@@ -131,11 +131,11 @@ public class HTMLCompactWeekView extends AbstractHTMLView {
         
         for (int row=0;row<rowsize;row++) {
             result.append("<tr>\n");
-            result.append("<td class=\"month_cell\" valign=\"top\" height=\"40\">\n");
+            result.append("<th class=\"month_header\" valign=\"top\" height=\"40\">\n");
             if ( slotNames.length > row ) {
                 result.append( slotNames[ row ] );
             }
-            result.append("</td>\n");
+            result.append("</th>\n");
             for (int column=0;column < columns; column++) {
                 int fieldNumber = row * columns + column;
                 if ( !isExcluded( column ) ) {
