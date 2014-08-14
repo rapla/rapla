34949524593a511f2cd66380dbae42491b2ad834
Christopher Kohlhaas <christopher.kohlhaas@gmail.com>
2014-08-14 12:24:14 +0200

    removed fixed JAVA_HOME from build.bat

--- build.bat
+++ build.bat
@@ -7,7 +7,6 @@
 :: for a list of all available build-targets type
 ::  .\build.bat -projecthelp 
 if not "%ANT_HOME%" =="" goto gotAntHome
-set JAVA_HOME=C:\entwicklung\Java\jdk1.7.0_51
 set ANT_HOME=.
 :gotAntHome
 call %ANT_HOME%\bin\ant.bat %1 %2 %3 %4 %5 %6
