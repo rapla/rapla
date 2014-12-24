475e7c574d42e2a2448aa706115e3f6a02e12821
Christopher Kohlhaas <christopher.kohlhaas@gmail.com>
2014-12-24 03:38:45 +0100

    extracted RaplaJDKLoggingAdapterWithoutClassnameSupport for GWT

--- src/org/rapla/framework/internal/RaplaJDKLoggingAdapter.java
+++ src/org/rapla/framework/internal/RaplaJDKLoggingAdapter.java
@@ -16,88 +16,27 @@ package org.rapla.framework.internal;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.inject.Provider;
 
+public class RaplaJDKLoggingAdapter extends RaplaJDKLoggingAdapterWithoutClassnameSupport {
+    private static String WRAPPER_NAME = RaplaJDKLoggingAdapter.class.getName();
 
-public class RaplaJDKLoggingAdapter implements Provider<org.rapla.framework.logger.Logger> {
-
-    public org.rapla.framework.logger.Logger get() {
-        final Logger logger = getLogger(  "rapla");
-        return new Wrapper(logger, "rapla");
-	}
-   
-	static private java.util.logging.Logger getLogger(String categoryName) 
-	{
-		Logger logger = Logger.getLogger(categoryName);
-		return logger;
-	}
-
-    static String WRAPPER_NAME = RaplaJDKLoggingAdapter.class.getName();
-
-    class Wrapper implements org.rapla.framework.logger.Logger{
-        java.util.logging.Logger logger;
-        String id;
-
-        public Wrapper( java.util.logging.Logger logger, String id) {
-            this.logger = logger;
-            this.id = id;
-        }
-
-        public boolean isDebugEnabled() {
-            return logger.isLoggable(Level.CONFIG);
-        }
-
-        public void debug(String message) {
-        	log(Level.CONFIG, message);
-        }
-
-        public void info(String message) {
-        	log(Level.INFO, message);
-        }
-
-        public void warn(String message) {
-            log(Level.WARNING,message);
-        }
-
-        public void warn(String message, Throwable cause) {
-            log(Level.WARNING,message, cause);
-        }
-
-        public void error(String message) {
-        	log(Level.SEVERE, message);
-        }
-
-        public void error(String message, Throwable cause) {
-        	log(Level.SEVERE, message, cause);
-        }
-
-		private void log(Level level,String message) {
-			log(level, message, null);
-		}
-
-		private void log(Level level,String message, Throwable cause) {
-			StackTraceElement[] stackTrace = new Throwable().getStackTrace();
-	        String sourceClass = null;
-	        String sourceMethod =null;
-			for (StackTraceElement element:stackTrace)
-            {
-                String classname = element.getClassName();
-                if ( !classname.startsWith(WRAPPER_NAME))
-                {
-                    sourceClass=classname;
-                    sourceMethod =element.getMethodName();
-                    break;
-                }
-            }
-            logger.logp(level,sourceClass, sourceMethod,message, cause);
-		}
-
-        public org.rapla.framework.logger.Logger getChildLogger(String childLoggerName) 
+    @Override
+    protected void log_(Logger logger, Level level, String message, Throwable cause) {
+        StackTraceElement[] stackTrace = new Throwable().getStackTrace();
+        String sourceClass = null;
+        String sourceMethod =null;
+        for (StackTraceElement element:stackTrace)
         {
-            String childId = id+ "." + childLoggerName;
-            Logger childLogger = getLogger( childId);
-            return new Wrapper( childLogger, childId);
+            String classname = element.getClassName();
+            if ( !classname.startsWith(WRAPPER_NAME))
+            {
+                sourceClass=classname;
+                sourceMethod =element.getMethodName();
+                break;
+            }
         }
+        logger.logp(level,sourceClass, sourceMethod,message, cause);
     }
+
 
 }
