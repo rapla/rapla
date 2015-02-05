01a86999b183080222882da27a55c85cb929ba3d
Christopher Kohlhaas <christopher.kohlhaas@gmail.com>
2015-02-05 02:20:06 -1100

    npe avoid just in case the client fails

--- src/org/rapla/client/MainWebstart.java
+++ src/org/rapla/client/MainWebstart.java
@@ -26,7 +26,10 @@ final public class MainWebstart
         	main.startRapla("client");
         } catch (Throwable ex) {
             main.getLogger().error("Couldn't start Rapla",ex);
-            main.raplaContainer.dispose();
+            if (main.raplaContainer != null)
+            {
+                main.raplaContainer.dispose();
+            }
             System.out.flush();
             try
             {
