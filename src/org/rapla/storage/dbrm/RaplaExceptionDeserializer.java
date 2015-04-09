434985dc96e31dfce1322efbe97a81d01b96debe
Martin Geuer <martin.geuer@gmail.com>
2015-04-09 14:50:07 +0200

    JSON exception deserializer ported to GWT

---
+++ src/org/rapla/storage/dbrm/RaplaExceptionDeserializer.java
@@ -0,0 +1,67 @@
+package org.rapla.storage.dbrm;
+
+import java.util.List;
+
+import org.rapla.entities.DependencyException;
+import org.rapla.entities.EntityNotFoundException;
+import org.rapla.framework.RaplaException;
+import org.rapla.framework.RaplaSynchronizationException;
+import org.rapla.storage.RaplaNewVersionException;
+import org.rapla.storage.RaplaSecurityException;
+
+public class RaplaExceptionDeserializer {
+	public RaplaException deserializeException(String classname, String message, List<String> params) 
+    {
+    	String error = "";
+    	if ( message != null)
+    	{
+    		error+=message;
+    	}
+	    if ( classname != null)
+	    {
+            if ( classname.equals( WrongRaplaVersionException.class.getName()))
+            {
+                return new WrongRaplaVersionException( message);
+            }
+            else if ( classname.equals(RaplaNewVersionException.class.getName()))
+            {
+                return new RaplaNewVersionException( message);
+            }
+            else if ( classname.equals( RaplaSecurityException.class.getName()))
+            {
+                return new RaplaSecurityException( message);
+            }
+            else if ( classname.equals( RaplaSynchronizationException.class.getName()))
+            {
+                return new RaplaSynchronizationException( message);
+            }
+            else if ( classname.equals( RaplaConnectException.class.getName()))
+            {
+                return new RaplaConnectException( message);
+            }
+            else if ( classname.equals( EntityNotFoundException.class.getName()))
+            {
+//                    if ( param != null)
+//                    {
+//                            String id = (String)convertFromString( String.class, param);
+//                            return new EntityNotFoundException( message, id);
+//                    }
+                return new EntityNotFoundException( message);
+            }
+            else if ( classname.equals( DependencyException.class.getName()))
+            {
+                if ( params != null)
+                {
+                	return new DependencyException( message,params);
+                }
+                //Collection<String> depList = Collections.emptyList();
+                return new DependencyException( message, new String[] {});
+            }
+            else
+            {
+                    error = classname + " " + error;
+            }
+	    }
+	    return new RaplaException( error);
+    }
+}
--- src/org/rapla/storage/dbrm/RaplaHTTPConnector.java
+++ src/org/rapla/storage/dbrm/RaplaHTTPConnector.java
@@ -135,7 +135,7 @@ public class RaplaHTTPConnector extends HTTPJsonConnector
 						params.add(param.toString());
 					}
 				}
-				RaplaException ex = deserializeException(classname, message.toString(), params);
+				RaplaException ex = new RaplaExceptionDeserializer().deserializeException(classname, message.toString(), params);
 				try
 				{
     				if ( stacktrace != null)
@@ -429,62 +429,6 @@ public class RaplaHTTPConnector extends HTTPJsonConnector
         element.addProperty("id", "1");
         return element;
     }
-    
-    public RaplaException deserializeException(String classname, String message, List<String> params) 
-    {
-    	String error = "";
-    	if ( message != null)
-    	{
-    		error+=message;
-    	}
-	    if ( classname != null)
-	    {
-            if ( classname.equals( WrongRaplaVersionException.class.getName()))
-            {
-                return new WrongRaplaVersionException( message);
-            }
-            else if ( classname.equals(RaplaNewVersionException.class.getName()))
-            {
-                return new RaplaNewVersionException( message);
-            }
-            else if ( classname.equals( RaplaSecurityException.class.getName()))
-            {
-                return new RaplaSecurityException( message);
-            }
-            else if ( classname.equals( RaplaSynchronizationException.class.getName()))
-            {
-                return new RaplaSynchronizationException( message);
-            }
-            else if ( classname.equals( RaplaConnectException.class.getName()))
-            {
-                return new RaplaConnectException( message);
-            }
-            else if ( classname.equals( EntityNotFoundException.class.getName()))
-            {
-//                    if ( param != null)
-//                    {
-//                            String id = (String)convertFromString( String.class, param);
-//                            return new EntityNotFoundException( message, id);
-//                    }
-                return new EntityNotFoundException( message);
-            }
-            else if ( classname.equals( DependencyException.class.getName()))
-            {
-                if ( params != null)
-                {
-                	return new DependencyException( message,params);
-                }
-                //Collection<String> depList = Collections.emptyList();
-                return new DependencyException( message, new String[] {});
-            }
-            else
-            {
-                    error = classname + " " + error;
-            }
-	    }
-	    return new RaplaException( error);
-    }
-
     
     class ReconnectInfo
     {
