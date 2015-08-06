9eeb3a765deab831c30dbacbcaf8726163fe73ce
Christopher Kohlhaas <christopher.kohlhaas@gmail.com>
2015-08-06 16:06:33 +0200

    Configurable summary field for ical

--- src/org/rapla/client/internal/RaplaClientServiceImpl.java
+++ src/org/rapla/client/internal/RaplaClientServiceImpl.java
@@ -100,6 +100,7 @@ import org.rapla.gui.internal.edit.annotation.ConflictCreationAnnotationEdit;
 import org.rapla.gui.internal.edit.annotation.EmailAnnotationEdit;
 import org.rapla.gui.internal.edit.annotation.ExpectedColumnsAnnotationEdit;
 import org.rapla.gui.internal.edit.annotation.ExpectedRowsAnnotationEdit;
+import org.rapla.gui.internal.edit.annotation.ExportEventDescriptionAnnotationEdit;
 import org.rapla.gui.internal.edit.annotation.ExportEventNameAnnotationEdit;
 import org.rapla.gui.internal.edit.annotation.LocationAnnotationEdit;
 import org.rapla.gui.internal.edit.annotation.ResourceTreeNameAnnotationEdit;
@@ -237,6 +238,7 @@ public class RaplaClientServiceImpl extends ContainerImpl implements ClientServi
         addContainerProvidedComponent( AnnotationEditExtension.DYNAMICTYPE_ANNOTATION_EDIT, ConflictCreationAnnotationEdit.class);
         addContainerProvidedComponent( AnnotationEditExtension.DYNAMICTYPE_ANNOTATION_EDIT, ResourceTreeNameAnnotationEdit.class);
         addContainerProvidedComponent( AnnotationEditExtension.DYNAMICTYPE_ANNOTATION_EDIT, ExportEventNameAnnotationEdit.class);
+        addContainerProvidedComponent( AnnotationEditExtension.DYNAMICTYPE_ANNOTATION_EDIT, ExportEventDescriptionAnnotationEdit.class);
         
         frameControllerList = new FrameControllerList(getLogger().getChildLogger("framelist"));
         addContainerProvidedComponentInstance(FrameControllerList.class,frameControllerList);
--- src/org/rapla/entities/dynamictype/DynamicTypeAnnotations.java
+++ src/org/rapla/entities/dynamictype/DynamicTypeAnnotations.java
@@ -17,6 +17,7 @@ public interface DynamicTypeAnnotations
     String KEY_NAME_FORMAT="nameformat";
     String KEY_NAME_FORMAT_PLANNING="nameformat_planning";
     String KEY_NAME_FORMAT_EXPORT="nameformat_export";
+    String KEY_DESCRIPTION_FORMAT_EXPORT="descriptionformat_export";
     
     String KEY_CLASSIFICATION_TYPE="classification-type";
     String VALUE_CLASSIFICATION_TYPE_RESOURCE="resource";
