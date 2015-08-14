28fd9776942dfb19b0cb15931b7810212563f9ed
Martin Geuer <martin.geuer@erdkante.de>
2015-08-14 11:23:13 +0200

    implemented toString method for gwt locale

--- src/main/gwt-jre/jre/java/util/Locale.java
+++ src/main/gwt-jre/jre/java/util/Locale.java
@@ -2,46 +2,54 @@ package java.util;
 
 import java.util.Locale;
 
-public class Locale {
+public class Locale
+{
     static public final Locale GERMANY = createConstants("de", "DE");
     static public final Locale GERMAN = createConstants("de", "");
     static public final Locale ENGLISH = createConstants("en", "");
     String lang;
     String country;
     String variant;
-    
+
     public Locale(String lang)
     {
         this(lang, null);
     }
-    public Locale(String lang,String country)
+
+    public Locale(String lang, String country)
     {
         this(lang, country, null);
     }
-    public Locale(String lang,String country, String variant)
+
+    public Locale(String lang, String country, String variant)
     {
         this.lang = lang;
         this.country = country;
         this.variant = variant;
     }
-    
+
     public String getLanguage()
     {
-    	return lang;
+        return lang;
     }
-    
+
     public String getCountry()
     {
-    	return country;
+        return country;
     }
-    
+
     public static Locale getDefault()
     {
-    	return ENGLISH;
+        return ENGLISH;
     }
-    
+
+    public String toString()
+    {
+        return lang + (country != null && !country.trim().isEmpty() ? "_" + country : "");
+    }
+
     private static Locale createConstants(String lang, String country)
     {
-    	return new Locale(lang,country);
+        return new Locale(lang, country);
     }
 }
