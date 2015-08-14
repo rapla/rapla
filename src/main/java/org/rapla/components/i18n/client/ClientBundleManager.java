94750e39f2fb51897c09b5e0051800f68f67a796
Martin Geuer <martin.geuer@erdkante.de>
2015-08-14 10:45:38 +0200

    simple implementation for gwt: load on creation the languages and block until finished

--- src/main/java/org/rapla/components/i18n/client/ClientBundleManager.java
+++ src/main/java/org/rapla/components/i18n/client/ClientBundleManager.java
@@ -2,6 +2,7 @@ package org.rapla.components.i18n.client;
 
 import java.util.Collection;
 import java.util.Locale;
+import java.util.Map;
 
 import org.rapla.components.i18n.BundleManager;
 import org.rapla.components.i18n.I18nLocaleFormats;
@@ -11,35 +12,47 @@ import org.rapla.storage.dbrm.RemoteServer;
 
 import com.google.inject.Inject;
 
-public class ClientBundleManager implements BundleManager {
-    private final RemoteServer remoteServer;
+public class ClientBundleManager implements BundleManager
+{
     private final LocaleSelector selector;
     LocalePackage localePackage;
 
     @Inject
-    protected ClientBundleManager(LocaleSelector selector, RemoteServer remoteServer) {
-        this.remoteServer = remoteServer;
-        this.selector=selector;
+    protected ClientBundleManager(LocaleSelector selector, RemoteServer remoteServer)
+    {
+        this.selector = selector;
+        try
+        {
+            localePackage = remoteServer.locale("123", getLocale().toString()).get();
+        }
+        catch (Exception e)
+        {
+            throw new IllegalStateException("Could not load language pack");
+        }
     }
 
-
     @Override
-    public String format(String string, Object[] obj) {
+    public String format(String string, Object[] obj)
+    {
         // TODO
-//        final MessageFormat messageFormat = new MessageFormat(string);
-//        final String format = messageFormat.format(obj);
-//        return format;
+        //        final MessageFormat messageFormat = new MessageFormat(string);
+        //        final String format = messageFormat.format(obj);
+        //        return format;
         return string;
     }
 
     @Override
-    public String getString(String packageId,String key, Locale locale) {
-        return getString( packageId, key);
+    public String getString(String packageId, String key, Locale locale)
+    {
+        return getString(packageId, key);
     }
 
     @Override
-    public String getString(String packageId, String key) {
-        String result = localePackage.getBundles().get( packageId).get(key);
+    public String getString(String packageId, String key)
+    {
+        final Map<String, Map<String, String>> bundles = localePackage.getBundles();
+        final Map<String, String> language = bundles.get(packageId);
+        String result = language.get(key);
         return result;
     }
 
@@ -50,14 +63,14 @@ public class ClientBundleManager implements BundleManager {
     }
 
     @Override
-    public Collection<String> getKeys(String packageId) {
-        return localePackage.getBundles().get( packageId).keySet();
+    public Collection<String> getKeys(String packageId)
+    {
+        return localePackage.getBundles().get(packageId).keySet();
     }
-
 
     @Override
     public I18nLocaleFormats getFormats()
     {
-        return null;
+        return localePackage.getFormats();
     }
 }
