5650d9097e24cd466cdf07ea8338e269027aa01f
Martin Geuer <martin.geuer@erdkante.de>
2015-08-25 13:23:12 +0200

    listening for data is working

--- src/main/java/org/rapla/client/gwt/components/TreeComponent.java
+++ src/main/java/org/rapla/client/gwt/components/TreeComponent.java
@@ -1,34 +1,38 @@
 package org.rapla.client.gwt.components;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 import org.gwtbootstrap3.client.ui.html.Div;
 import org.rapla.entities.domain.Allocatable;
 
 import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.core.client.JsArrayInteger;
+import com.google.gwt.core.client.JsArrayString;
 import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.core.client.Scheduler.ScheduledCommand;
 import com.google.gwt.json.client.JSONArray;
+import com.google.gwt.json.client.JSONNumber;
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.json.client.JSONString;
-import com.google.gwt.json.client.JSONValue;
 import com.google.gwt.user.client.Window;
 
 public class TreeComponent extends Div
 {
     private static int counter = 0;
-    public TreeComponent(Allocatable[] allocatables, Locale locale)
+    private Allocatable[] allocatables;
+    public TreeComponent(final Allocatable[] allocatables, Locale locale)
     {
+        this.allocatables = allocatables;
         final String id = "tree-"+counter;
         this.setId(id);
         final JSONArray data = new JSONArray();
-        int i = 0;
-        for (Allocatable allocatable : allocatables)
+        for (int i = 0; i < allocatables.length; i++)
         {
             final JSONObject obj = new JSONObject();
-            data.set(i++, obj);
-            obj.put("id", new JSONString(allocatable.getId()));
-            obj.put("text", new JSONString(allocatable.getName(locale)));
+            data.set(i, obj);
+            obj.put("id", new JSONNumber(i+1));
+            obj.put("text", new JSONString(allocatables[i].getName(locale)));
         }
         // switch the layout after loading 
         Scheduler.get().scheduleFinally(new ScheduledCommand()
@@ -43,7 +47,17 @@ public class TreeComponent extends Div
     
     private void selectionChanged(Object selected)
     {
-        Window.alert("Selected:"+selected);
+        if (selected instanceof JsArrayInteger)
+        {
+            JsArrayInteger selectedPositions = ((JsArrayInteger) selected);
+            final ArrayList<Allocatable> selectedAllocatables = new ArrayList<Allocatable>();
+            for (int i = 0; i < selectedPositions.length(); i++)
+            {
+                final int selectedPosition = selectedPositions.get(i);
+                selectedAllocatables.add(allocatables[selectedPosition-1]);
+            }
+            Window.alert(selectedAllocatables.toString());
+        }
     }
     
     public native void initJs(final String id, final JavaScriptObject data, final TreeComponent tc)/*-{
