a86b72a120c8bc8270d7ac1139f72889c413b461
Martin Geuer <martin.geuer@erdkante.de>
2015-09-11 14:48:43 +0200

    used gin binder from rapla

--- src/main/java/org/rapla/client/gwt/Rapla.java
+++ src/main/java/org/rapla/client/gwt/Rapla.java
@@ -1,11 +1,14 @@
 package org.rapla.client.gwt;
 
+import org.rapla.inject.client.RaplaGinModules;
+
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 
 public class Rapla implements EntryPoint {
 
     public void onModuleLoad() {
+        GWT.create(RaplaGinModules.class);
         final MainInjector injector = GWT.create(MainInjector.class);
         new RaplaGwtStarter(injector).startApplication();
     }
