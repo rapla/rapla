faf7fcf3f613593272ea7243e508d3e0b6fa5bf1
Christopher Kohlhaas <christopher.kohlhaas@gmail.com>
2015-09-21 10:13:52 +0200

    beginn transition to new plugin api

--- src/main/java/org/rapla/gui/EventCheck.java
+++ src/main/java/org/rapla/gui/EventCheck.java
@@ -8,7 +8,7 @@ import org.rapla.inject.ExtensionPoint;
 import org.rapla.inject.InjectionContext;
 
 /** performs a check, if the reservation is entered correctly. An example of a reservation check is the conflict checker*/
-@ExtensionPoint(id="eventcheck",context = InjectionContext.client)
+@ExtensionPoint(id="eventcheck",context = InjectionContext.swing)
 public interface EventCheck 
 {
     /** @param sourceComponent 
