5361463c9eda3a6eea526b210a834f55bd7136e5
Martin Geuer <martin.geuer@erdkante.de>
2015-10-09 12:13:26 +0200

    removed unused extension point

--- src/main/java/org/rapla/client/extensionpoints/EditMenuExtension.java
+++ src/main/java/org/rapla/client/extensionpoints/EditMenuExtension.java
@@ -4,7 +4,7 @@ import org.rapla.gui.toolkit.IdentifiableMenuEntry;
 import org.rapla.inject.ExtensionPoint;
 import org.rapla.inject.InjectionContext;
 
-@ExtensionPoint(context = InjectionContext.swing,id="EditMenuInsert")
+@ExtensionPoint(context = InjectionContext.swing,id="org.rapla.gui.EditMenuInsert")
 public interface EditMenuExtension extends IdentifiableMenuEntry
 {
 }
