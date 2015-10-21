bdb47a53a8e175be00d1cfb78bb6cfdc4e27cc12
Martin Geuer <martin.geuer@erdkante.de>
2015-10-21 14:10:15 +0200

    RaplaGinModulesGenerated are now annotation processed generated

--- src/main/java/org/rapla/client/gwt/Rapla.java
+++ src/main/java/org/rapla/client/gwt/Rapla.java
@@ -1,7 +1,6 @@
 package org.rapla.client.gwt;
 
 import org.rapla.client.gwt.view.RaplaPopups;
-import org.rapla.inject.client.RaplaGinModules;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
@@ -10,7 +9,6 @@ public class Rapla implements EntryPoint {
 
     public void onModuleLoad() {
         RaplaPopups.getProgressBar().setPercent(10);
-        GWT.create(RaplaGinModules.class);
         final MainInjector injector = GWT.create(MainInjector.class);
         new RaplaGwtStarter(injector).startApplication();
     }
