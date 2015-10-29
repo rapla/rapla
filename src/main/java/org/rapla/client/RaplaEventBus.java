08ea139c21d04e31a41cffb0b75dab6ea4b4286f
Christopher Kohlhaas <christopher.kohlhaas@gmail.com>
2015-10-29 22:08:20 +0100

    eventbus singleton

--- src/main/java/org/rapla/client/RaplaEventBus.java
+++ src/main/java/org/rapla/client/RaplaEventBus.java
@@ -6,7 +6,9 @@ import org.rapla.inject.DefaultImplementation;
 import org.rapla.inject.InjectionContext;
 
 import javax.inject.Inject;
+import javax.inject.Singleton;
 
+@Singleton
 @DefaultImplementation(of=EventBus.class,context = InjectionContext.client)
 public class RaplaEventBus extends SimpleEventBus
 {
