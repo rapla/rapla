8a26f84af32d1f81bc2cdc6ff68d5596a5013d72
Martin Geuer <martin.geuer@erdkante.de>
2015-11-05 11:40:18 +0100

    set ServiceEntryPointFactory

--- src/main/java/org/rapla/client/swing/internal/dagger/DaggerClientCreator.java
+++ src/main/java/org/rapla/client/swing/internal/dagger/DaggerClientCreator.java
@@ -1,7 +1,11 @@
 package org.rapla.client.swing.internal.dagger;
 
+import java.net.URL;
+
 import org.rapla.client.internal.RaplaClientServiceImpl;
 import org.rapla.framework.StartupEnvironment;
+import org.rapla.jsonrpc.client.EntryPointFactory;
+import org.rapla.jsonrpc.client.swing.BasicRaplaHTTPConnector;
 
 public class DaggerClientCreator
 {
@@ -9,6 +13,15 @@ public class DaggerClientCreator
     {
         ClientComponent component= org.rapla.client.swing.internal.dagger.DaggerClientComponent.builder().myClientModule(new MyClientModule(startupEnvironment)).build();
         final RaplaClientServiceImpl client = component.getClient();
+        URL downloadURL = startupEnvironment.getDownloadURL();
+        BasicRaplaHTTPConnector.setServiceEntryPointFactory(new EntryPointFactory()
+        {
+            @Override public String getEntryPoint(String interfaceName, String relativePath)
+            {
+                String url = downloadURL.toExternalForm()  + "rapla/json/" +((relativePath != null) ? relativePath: interfaceName);
+                return url;
+            }
+        });
         //server.getMethodProvider().setList( component.getWebservices().getList());
         return client;
     }
