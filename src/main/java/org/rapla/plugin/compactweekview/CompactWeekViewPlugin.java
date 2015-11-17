f1d4d69eabfcf438de1faed94e170549a52b8cd2
mgeuer <martin.geuer@erdkante.de>
2015-11-17 11:06:31 +0100

    Delete CompactWeekViewPlugin.java

--- src/main/java/org/rapla/plugin/compactweekview/CompactWeekViewPlugin.java
+++
@@ -1,9 +0,0 @@
-package org.rapla.plugin.compactweekview;
-
-/**
- * Created by Christopher on 22.09.2015.
- */
-public interface CompactWeekviewPlugin
-{
-    String COMPACT_WEEK_VIEW = "week_compact";
-}
