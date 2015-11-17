38a4399798e7316f7595e9267fe7a150e62891aa
Christopher Kohlhaas <christopher.kohlhaas@gmail.com>
2015-11-17 11:34:45 +0100

    added again with correct name

---
+++ src/main/java/org/rapla/plugin/compactweekview/CompactWeekviewPlugin.java
@@ -0,0 +1,10 @@
+package org.rapla.plugin.compactweekview;
+
+import org.rapla.entities.configuration.RaplaConfiguration;
+import org.rapla.framework.TypedComponentRole;
+
+public interface CompactWeekviewPlugin
+{
+    String COMPACT_WEEK_VIEW = "week_compact";
+    public static final TypedComponentRole<RaplaConfiguration> CONFIG = new TypedComponentRole<RaplaConfiguration>(COMPACT_WEEK_VIEW + ".config");
+}
