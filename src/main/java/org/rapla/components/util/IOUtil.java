7beac1b70046fd65fa681ca5be3ab21ec60539ef
Martin Geuer <martin.geuer@erdkante.de>
2015-12-09 15:48:46 +0100

    extracted reading bytes from input stream

--- src/main/java/org/rapla/components/util/IOUtil.java
+++ src/main/java/org/rapla/components/util/IOUtil.java
@@ -58,21 +58,26 @@ abstract public class IOUtil {
     /** reads the content form an url into a ByteArray*/
     public static byte[] readBytes(URL url) throws IOException {
         InputStream in = null;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
         try {
             in = url.openStream();
-            byte[] buffer = new byte[1024];
-            int count = 0;
-            do {
-                out.write(buffer, 0, count);
-                count = in.read(buffer, 0, buffer.length);
-            } while (count != -1);
-            return out.toByteArray();
+            return readBytes(in);
         } finally {
             if ( in != null) {
                 in.close();
             } // end of if ()
         }
+    }
+
+    public static byte[] readBytes(InputStream in) throws IOException
+    {
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int count = 0;
+        do {
+            out.write(buffer, 0, count);
+            count = in.read(buffer, 0, buffer.length);
+        } while (count != -1);
+        return out.toByteArray();
     }
 
 
