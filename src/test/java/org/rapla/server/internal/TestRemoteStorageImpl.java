287fb975992cf30759deaedcfedb9d6fe393e732
Martin Geuer <martin.geuer@erdkante.de>
2016-01-27 10:02:42 +0100

    test for categories store and remove

---
+++ src/test/java/org/rapla/server/internal/TestRemoteStorageImpl.java
@@ -0,0 +1,175 @@
+package org.rapla.server.internal;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
+import javax.inject.Provider;
+
+import org.eclipse.jetty.server.Server;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.rapla.ServletTestBase;
+import org.rapla.entities.Category;
+import org.rapla.entities.Entity;
+import org.rapla.facade.ClientFacade;
+import org.rapla.facade.ModificationEvent;
+import org.rapla.facade.ModificationListener;
+import org.rapla.facade.RaplaFacade;
+import org.rapla.framework.RaplaException;
+import org.rapla.framework.logger.Logger;
+import org.rapla.test.util.RaplaTestCase;
+
+@RunWith(JUnit4.class)
+public class TestRemoteStorageImpl
+{
+
+    private Server server;
+
+    private ClientFacade clientFacade;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        Logger logger = RaplaTestCase.initLoger();
+        int port = 8052;
+        ServerContainerContext container = new ServerContainerContext();
+        String xmlFile = "testdefault.xml";
+        container.setFileDatasource(RaplaTestCase.getTestDataFile(xmlFile));
+        ServerServiceImpl serverService = (ServerServiceImpl) RaplaTestCase.createServer(logger, container);
+        server = ServletTestBase.createServer(serverService, port);
+        Provider<ClientFacade> clientFacadeProvider = RaplaTestCase.createFacadeWithRemote(logger, port);
+        clientFacade = clientFacadeProvider.get();
+        clientFacade.login("homer", "duffs".toCharArray());
+    }
+
+    @After
+    public void cleanup() throws Exception
+    {
+        server.stop();
+    }
+
+    @Test
+    public void testAddCategoriesUnordered() throws Exception
+    {
+        Collection<Category> toStore = new ArrayList<Category>();
+        final RaplaFacade raplaFacade = clientFacade.getRaplaFacade();
+        final Category newCategory = raplaFacade.newCategory();
+        newCategory.setKey("newCategory");
+        final Category superCategory = raplaFacade.edit(raplaFacade.getSuperCategory());
+        superCategory.addCategory(newCategory);
+        final Category newChild = raplaFacade.newCategory();
+        newChild.setKey("newChild");
+        newCategory.addCategory(newChild);
+        toStore.add(superCategory);
+        toStore.add(newChild);
+        toStore.add(newCategory);
+        raplaFacade.storeAndRemove(toStore.toArray(new Entity[0]), Entity.ENTITY_ARRAY, clientFacade.getUser());
+    }
+
+    @Test
+    public void testDeleteCategoryWithParentSend() throws Exception
+    {
+        final RaplaFacade raplaFacade = clientFacade.getRaplaFacade();
+        {// init test data
+            Collection<Category> toStore = new ArrayList<Category>();
+            final Category newCategory = raplaFacade.newCategory();
+            newCategory.setKey("newCategory");
+            final Category superCategory = raplaFacade.edit(raplaFacade.getSuperCategory());
+            superCategory.addCategory(newCategory);
+            final Category newChild = raplaFacade.newCategory();
+            newChild.setKey("newChild");
+            newCategory.addCategory(newChild);
+            toStore.add(superCategory);
+            toStore.add(newCategory);
+            toStore.add(newChild);
+            raplaFacade.storeAndRemove(toStore.toArray(new Entity[0]), Entity.ENTITY_ARRAY, clientFacade.getUser());
+
+        }
+        // register mod listener so we can see if cat was deleted
+        final Semaphore waitFor = new Semaphore(0);
+        final AtomicReference<ModificationEvent> eventRef = new AtomicReference<ModificationEvent>(null);
+        clientFacade.addModificationListener(new ModificationListener()
+        {
+            @Override
+            public void dataChanged(ModificationEvent evt) throws RaplaException
+            {
+                eventRef.set(evt);
+                waitFor.release();
+            }
+        });
+        // now delete parent, so category with key newCategory
+        final Category parentCat = raplaFacade.edit(raplaFacade.getSuperCategory().getCategory("newCategory"));
+        final Category toDelCat = parentCat.getCategory("newChild");
+        parentCat.removeCategory(toDelCat);
+        raplaFacade.store(parentCat);
+        final boolean tryAcquire = waitFor.tryAcquire(3, TimeUnit.SECONDS);
+        Assert.assertTrue(tryAcquire);
+        final ModificationEvent modificationEvent = eventRef.get();
+        Assert.assertEquals(parentCat.getId(), modificationEvent.getChanged().iterator().next().getId());
+        Assert.assertEquals(toDelCat.getId(), modificationEvent.getRemovedReferences().iterator().next().getId());
+    }
+
+    @Test
+    public void testDeleteCategoryWithDeletedSend() throws Exception
+    {
+        final RaplaFacade raplaFacade = clientFacade.getRaplaFacade();
+        {// init test data
+            Collection<Category> toStore = new ArrayList<Category>();
+            final Category newCategory = raplaFacade.newCategory();
+            newCategory.setKey("newCategory");
+            final Category superCategory = raplaFacade.edit(raplaFacade.getSuperCategory());
+            superCategory.addCategory(newCategory);
+            final Category newChild = raplaFacade.newCategory();
+            newChild.setKey("newChild");
+            newCategory.addCategory(newChild);
+            toStore.add(superCategory);
+            toStore.add(newCategory);
+            toStore.add(newChild);
+            raplaFacade.storeAndRemove(toStore.toArray(new Entity[0]), Entity.ENTITY_ARRAY, clientFacade.getUser());
+
+        }
+        // register mod listener so we can see if cat was deleted
+        final Semaphore waitFor = new Semaphore(0);
+        final AtomicReference<ModificationEvent> eventRef = new AtomicReference<ModificationEvent>(null);
+        clientFacade.addModificationListener(new ModificationListener()
+        {
+            @Override
+            public void dataChanged(ModificationEvent evt) throws RaplaException
+            {
+                eventRef.set(evt);
+                waitFor.release();
+            }
+        });
+        // now delete parent, so category with key newCategory
+        final Category parentCat = raplaFacade.edit(raplaFacade.getSuperCategory().getCategory("newCategory"));
+        final Category toDelCat = parentCat.getCategory("newChild");
+        raplaFacade.remove(toDelCat);
+        final boolean tryAcquire = waitFor.tryAcquire(3, TimeUnit.SECONDS);
+        Assert.assertTrue(tryAcquire);
+        final ModificationEvent modificationEvent = eventRef.get();
+        Assert.assertEquals(parentCat.getId(), modificationEvent.getChanged().iterator().next().getId());
+        Assert.assertEquals(toDelCat.getId(), modificationEvent.getRemovedReferences().iterator().next().getId());
+    }
+
+    @Test
+    public void testInsertChildCategory() throws Exception
+    {
+        final RaplaFacade raplaFacade = clientFacade.getRaplaFacade();
+        Collection<Category> toStore = new ArrayList<Category>();
+        final Category newCategory = raplaFacade.newCategory();
+        newCategory.setKey("newCategory");
+        final Category superCategory = raplaFacade.edit(raplaFacade.getSuperCategory());
+        superCategory.addCategory(newCategory);
+        toStore.add(newCategory);
+        raplaFacade.storeAndRemove(toStore.toArray(new Entity[0]), Entity.ENTITY_ARRAY, clientFacade.getUser());
+        // check if we can resolve the new category
+        Assert.assertNotNull(raplaFacade.getSuperCategory().getCategory("newCategory"));
+    }
+}
