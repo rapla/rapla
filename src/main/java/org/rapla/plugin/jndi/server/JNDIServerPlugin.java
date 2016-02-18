2f1b43beb85762540941aa572e9d8fd35aacb8a5
Martin Geuer <martin.geuer@erdkante.de>
2016-02-18 15:51:15 +0100

    moved convert method into JNDIServerPlugin

--- src/main/java/org/rapla/plugin/jndi/server/JNDIServerPlugin.java
+++ src/main/java/org/rapla/plugin/jndi/server/JNDIServerPlugin.java
@@ -13,12 +13,16 @@
 package org.rapla.plugin.jndi.server;
 
 import org.rapla.entities.configuration.RaplaConfiguration;
+import org.rapla.entities.configuration.internal.PreferencesImpl;
+import org.rapla.facade.RaplaComponent;
 import org.rapla.facade.RaplaFacade;
-import org.rapla.facade.internal.FacadeImpl;
 import org.rapla.framework.Configuration;
+import org.rapla.framework.DefaultConfiguration;
+import org.rapla.framework.RaplaException;
 import org.rapla.framework.TypedComponentRole;
 import org.rapla.framework.logger.Logger;
 import org.rapla.plugin.jndi.JNDIPlugin;
+import org.rapla.storage.xml.RaplaXMLContextException;
 
 public class JNDIServerPlugin  {
     
@@ -28,7 +32,51 @@ public class JNDIServerPlugin  {
         TypedComponentRole<RaplaConfiguration> newConfKey = JNDIPlugin.JNDISERVER_CONFIG;
         if ( config.getAttributeNames().length > 2)
         {
-            FacadeImpl.convertToNewPluginConfig(facade, logger, className, newConfKey);
+            JNDIServerPlugin.convertToNewPluginConfig(facade, logger, className, newConfKey);
+        }
+    }
+
+    static public void convertToNewPluginConfig(RaplaFacade facade, Logger logger, String className, TypedComponentRole<RaplaConfiguration> newConfKey)
+            throws RaplaXMLContextException
+    {
+        try
+        {
+            PreferencesImpl clone = (PreferencesImpl) facade.edit(facade.getSystemPreferences());
+            RaplaConfiguration entry = clone.getEntry(RaplaComponent.PLUGIN_CONFIG, null);
+            if (entry == null)
+            {
+                return;
+            }
+            RaplaConfiguration newPluginConfigEntry = entry.clone();
+            DefaultConfiguration pluginConfig = (DefaultConfiguration) newPluginConfigEntry.find("class", className);
+            // we split the config entry in the plugin config and the new config entry;
+            if (pluginConfig != null)
+            {
+                logger.info("Converting plugin conf " + className + " to preference entry " + newConfKey);
+                newPluginConfigEntry.removeChild(pluginConfig);
+                boolean enabled = pluginConfig.getAttributeAsBoolean("enabled", false);
+                RaplaConfiguration newPluginConfig = new RaplaConfiguration(pluginConfig.getName());
+                newPluginConfig.setAttribute("enabled", enabled);
+                newPluginConfig.setAttribute("class", className);
+                newPluginConfigEntry.addChild(newPluginConfig);
+    
+                RaplaConfiguration newConfigEntry = new RaplaConfiguration(pluginConfig);
+    
+                newConfigEntry.setAttribute("enabled", null);
+                newConfigEntry.setAttribute("class", null);
+    
+                clone.putEntry(newConfKey, newConfigEntry);
+                clone.putEntry(RaplaComponent.PLUGIN_CONFIG, newPluginConfigEntry);
+                facade.store(clone);
+            }
+        }
+        catch (RaplaException ex)
+        {
+            if (ex instanceof RaplaXMLContextException)
+            {
+                throw ex;
+            }
+            throw new RaplaXMLContextException(ex.getMessage(), ex);
         }
     }
     
