f0c26fe8f015719aaf21737d3df929e52bca6a6f
Christopher Kohlhaas <christopher.kohlhaas@gmail.com>
2016-02-29 10:50:51 +0100

    merged JNLP Shortcut from Christopher

--- src/main/java/org/rapla/server/servletpages/RaplaJNLPPageGenerator.java
+++ src/main/java/org/rapla/server/servletpages/RaplaJNLPPageGenerator.java
@@ -3,14 +3,13 @@
  */
 package org.rapla.server.servletpages;
 
-import org.rapla.RaplaResources;
-import org.rapla.components.util.DateTools;
-import org.rapla.components.util.IOUtil;
-import org.rapla.facade.RaplaFacade;
-import org.rapla.framework.RaplaException;
-import org.rapla.framework.internal.ContainerImpl;
-import org.rapla.inject.dagger.DaggerReflectionStarter;
-import org.rapla.server.ServerServiceContainer;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -21,96 +20,107 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+
+import org.rapla.RaplaResources;
+import org.rapla.components.util.DateTools;
+import org.rapla.components.util.IOUtil;
+import org.rapla.entities.configuration.Preferences;
+import org.rapla.facade.RaplaFacade;
+import org.rapla.framework.RaplaException;
+import org.rapla.framework.TypedComponentRole;
+import org.rapla.framework.internal.ContainerImpl;
+import org.rapla.inject.dagger.DaggerReflectionStarter;
+import org.rapla.server.ServerServiceContainer;
 
 @Path("raplaclient.jnlp")
 @Singleton
-public class RaplaJNLPPageGenerator  {
+public class RaplaJNLPPageGenerator
+{
 
+    private static final TypedComponentRole<Boolean> CREATE_SHORTCUT = new TypedComponentRole<Boolean>("org.rapla.jnlp.createshortcut");
     private final RaplaFacade facade;
     private final RaplaResources i18n;
     private final String moduleId;
 
     @Inject
-    public RaplaJNLPPageGenerator( RaplaFacade facade,RaplaResources i18n )
+    public RaplaJNLPPageGenerator(RaplaFacade facade, RaplaResources i18n)
     {
         this.facade = facade;
         this.i18n = i18n;
         moduleId = DaggerReflectionStarter.loadModuleId(ServerServiceContainer.class.getClassLoader());
     }
 
-    private String getCodebase( HttpServletRequest request)  {
+    private String getCodebase(HttpServletRequest request)
+    {
         StringBuffer codebaseBuffer = new StringBuffer();
         String forwardProto = request.getHeader("X-Forwarded-Proto");
-        boolean secure = (forwardProto != null && forwardProto.toLowerCase().equals("https"))  || request.isSecure();
+        boolean secure = (forwardProto != null && forwardProto.toLowerCase().equals("https")) || request.isSecure();
         codebaseBuffer.append(secure ? "https://" : "http://");
         codebaseBuffer.append(request.getServerName());
         if (request.getServerPort() != (!secure ? 80 : 443))
         {
-           codebaseBuffer.append(':');
-           codebaseBuffer.append(request.getServerPort());
+            codebaseBuffer.append(':');
+            codebaseBuffer.append(request.getServerPort());
         }
         codebaseBuffer.append(request.getContextPath());
         codebaseBuffer.append('/');
         return codebaseBuffer.toString();
     }
 
-    private String getLibsJNLP(ServletContext context, String webstartRoot) throws java.io.IOException {
+    private String getLibsJNLP(ServletContext context, String webstartRoot) throws java.io.IOException
+    {
         List<String> list = getClientLibs(context);
-        
+
         StringBuffer buf = new StringBuffer();
-        for (String file:list) {
-              buf.append("\n<jar href=\""+webstartRoot + "/");
-              buf.append(file);
-              buf.append("\"");
-              if (file.indexOf("/rapla-client.jar") >= 0) {
-                 buf.append(" main=\"true\"");
-              }
-              buf.append("/>");
-           }
+        for (String file : list)
+        {
+            buf.append("\n<jar href=\"" + webstartRoot + "/");
+            buf.append(file);
+            buf.append("\"");
+            if (file.indexOf("/rapla-client.jar") >= 0)
+            {
+                buf.append(" main=\"true\"");
+            }
+            buf.append("/>");
+        }
 
         return buf.toString();
     }
 
-    public static List<String> getClientLibs(ServletContext context)
-            throws IOException {
+    public static List<String> getClientLibs(ServletContext context) throws IOException
+    {
         List<String> list = new ArrayList<String>();
         URL resource = RaplaJNLPPageGenerator.class.getResource("/clientlibs.properties");
         if (resource != null)
         {
-            byte[] bytes = IOUtil.readBytes( resource);
-            String string = new String( bytes);
-            
+            byte[] bytes = IOUtil.readBytes(resource);
+            String string = new String(bytes);
+
             String[] split = string.split(";");
-            for ( String file:split)
+            for (String file : split)
             {
-                list.add( "webclient/" + file);
+                list.add("webclient/" + file);
             }
         }
         else
         {
             String base = context.getRealPath(".");
-            if ( base != null)
+            if (base != null)
             {
                 java.io.File baseFile = new java.io.File(base);
-                java.io.File[] files = IOUtil.getJarFiles(base,"webclient");
-                for (File file:files) {
-                  String relativeURL = IOUtil.getRelativeURL(baseFile,file);
-                  list.add( relativeURL);
-               }
+                java.io.File[] files = IOUtil.getJarFiles(base, "webclient");
+                for (File file : files)
+                {
+                    String relativeURL = IOUtil.getRelativeURL(baseFile, file);
+                    list.add(relativeURL);
+                }
             }
         }
         int size = list.size();
-        for (int i=0;i< size;i++)
+        for (int i = 0; i < size; i++)
         {
             String entry = list.get(i);
-            if ( entry.indexOf("raplaclient")>=0)
+            if (entry.indexOf("raplaclient") >= 0)
             {
                 list.remove(i);
                 list.add(0, entry);
@@ -118,70 +128,78 @@ public class RaplaJNLPPageGenerator  {
         }
         return list;
     }
-    
-    protected List<String> getProgramArguments() {
+
+    protected List<String> getProgramArguments()
+    {
         List<String> list = new ArrayList<String>();
         return list;
     }
 
     @GET
     @Produces(MediaType.TEXT_HTML)
-    public void generatePage( HttpServletRequest request, HttpServletResponse response ) throws IOException {
+    public void generatePage(HttpServletRequest request, HttpServletResponse response) throws IOException
+    {
         java.io.PrintWriter out = response.getWriter();
         String webstartRoot = ".";
         long currentTimeMillis = System.currentTimeMillis();
-        response.setDateHeader("Last-Modified",currentTimeMillis);
+        response.setDateHeader("Last-Modified", currentTimeMillis);
         response.addDateHeader("Expires", currentTimeMillis + DateTools.MILLISECONDS_PER_MINUTE);
         response.addDateHeader("Date", currentTimeMillis);
         response.setHeader("Cache-Control", "no-cache");
         final String defaultTitle = i18n.getString("rapla.title");
         String menuName;
+        boolean createShortcut = true;
         try
         {
-            menuName= facade.getSystemPreferences().getEntryAsString(ContainerImpl.TITLE, defaultTitle);
+            final Preferences systemPreferences = facade.getSystemPreferences();
+            menuName = systemPreferences.getEntryAsString(ContainerImpl.TITLE, defaultTitle);
+            createShortcut = systemPreferences.getEntryAsBoolean(CREATE_SHORTCUT, true);
         }
-        catch (RaplaException e) {
+        catch (RaplaException e)
+        {
             menuName = defaultTitle;
         }
         response.setContentType("application/x-java-jnlp-file;charset=utf-8");
         out.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
         out.println("<jnlp spec=\"1.0+\" codebase=\"" + getCodebase(request) + "\" href=\"" + getCodebase(request) + "rapla/raplaclient.jnlp\" >");
         out.println("<information>");
-        out.println(" <title>"+menuName+"</title>");
+        out.println(" <title>" + menuName + "</title>");
         out.println(" <vendor>Rapla team</vendor>");
         out.println(" <homepage href=\"http://code.google.com/p/rapla/\"/>");
         out.println(" <description>Resource Scheduling Application</description>");
         // we changed the logo from .gif to .png to make it more sexy
         //differentiate between icon and splash because of different sizes!
-        out.println(" <icon kind=\"default\" href=\""+webstartRoot+"/webclient/rapla_64x64.png\" width=\"64\" height=\"64\"/> ");
-        out.println(" <icon kind=\"desktop\" href=\""+webstartRoot+"/webclient/rapla_128x128.png\" width=\"128\" height=\"128\"/> ");
-        out.println(" <icon kind=\"shortcut\" href=\""+webstartRoot+"/webclient/rapla_64x64.png\" width=\"64\" height=\"64\"/> ");
+        out.println(" <icon kind=\"default\" href=\"" + webstartRoot + "/webclient/rapla_64x64.png\" width=\"64\" height=\"64\"/> ");
+        out.println(" <icon kind=\"desktop\" href=\"" + webstartRoot + "/webclient/rapla_128x128.png\" width=\"128\" height=\"128\"/> ");
+        out.println(" <icon kind=\"shortcut\" href=\"" + webstartRoot + "/webclient/rapla_64x64.png\" width=\"64\" height=\"64\"/> ");
         // and here aswell
 
-
-        out.println(" <icon kind=\"splash\" href=\""+webstartRoot+ "/webclient/logo.png\"/> ");
+        out.println(" <icon kind=\"splash\" href=\"" + webstartRoot + "/webclient/logo.png\"/> ");
         out.println(" <update check=\"always\" policy=\"always\"/>");
-        out.println(" <shortcut online=\"true\">");
-        out.println("       <desktop/>");
-        out.println("       <menu submenu=\"" + menuName +  "\"/>");
-        out.println(" </shortcut>");
+        if (createShortcut)
+        {
+            out.println(" <shortcut online=\"true\">");
+            out.println("       <desktop/>");
+            out.println("       <menu submenu=\"" + menuName + "\"/>");
+            out.println(" </shortcut>");
+        }
         out.println("</information>");
         out.println("<security>");
         out.println("  <all-permissions/>");
         out.println("</security>");
         out.println("<resources>");
         out.println("  <j2se version=\"1.4+\"/>");
-        
+
         String passedUsername = request.getParameter("username");
-        if ( passedUsername != null)
+        if (passedUsername != null)
         {
             String usernameProperty = "jnlp.org.rapla.startupUser";
             String safeUsername = URLEncoder.encode(passedUsername, "UTF-8");
-            out.println("  <property name=\"" +usernameProperty +"\" value=\"" + safeUsername + "\"/>");
+            out.println("  <property name=\"" + usernameProperty + "\" value=\"" + safeUsername + "\"/>");
         }
         {
             String moduleIdProperty = "jnlp.org.rapla.moduleId";
-            out.println("  <property name=\"" +moduleIdProperty +"\" value=\"" + moduleId + "\"/>");
+            out.println("  <property name=\"" + moduleIdProperty + "\" value=\"" + moduleId + "\"/>");
         }
         out.println(getLibsJNLP(request.getServletContext(), webstartRoot));
         out.println("</resources>");
@@ -191,11 +209,9 @@ public class RaplaJNLPPageGenerator  {
             out.println("  <argument>" + it.next() + "</argument> ");
         }
         out.println("</application-desc>");
-        
 
         out.println("</jnlp>");
         out.close();
-     }
-    
-    
+    }
+
 }
