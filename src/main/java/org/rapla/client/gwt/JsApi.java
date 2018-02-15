73de9aee215c355f23022159cc85e7dd7549ced6
Fabian Tarrach <33595266+ftarrach@users.noreply.github.com>
2018-02-15 22:37:16 +0100

    getCalendarNames in JsApi

--- src/main/java/org/rapla/client/gwt/JsApi.java
+++ src/main/java/org/rapla/client/gwt/JsApi.java
@@ -6,21 +6,26 @@ import org.rapla.RaplaResources;
 import org.rapla.client.ReservationController;
 import org.rapla.client.menu.RaplaObjectActions;
 import org.rapla.entities.User;
+import org.rapla.entities.configuration.CalendarModelConfiguration;
+import org.rapla.entities.configuration.Preferences;
+import org.rapla.facade.CalendarOptions;
 import org.rapla.facade.CalendarSelectionModel;
+import org.rapla.facade.RaplaComponent;
 import org.rapla.facade.RaplaFacade;
 import org.rapla.facade.client.ClientFacade;
 import org.rapla.framework.RaplaException;
 import org.rapla.framework.RaplaLocale;
 import org.rapla.logger.Logger;
 import org.rapla.plugin.abstractcalendar.RaplaBuilder;
+import org.rapla.plugin.autoexport.AutoExportPlugin;
 import org.rapla.storage.dbrm.RemoteAuthentificationService;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
+import java.util.Map;
 
 @JsType
-public class JsApi
-{
+public class JsApi {
     private final RaplaFacade facade;
     private final Logger logger;
     ReservationController reservationController;
@@ -36,8 +41,7 @@ public class JsApi
     @Inject
     public JsApi(ClientFacade facade, Logger logger, ReservationController reservationController, CalendarSelectionModel calendarModel,
                  RemoteAuthentificationService remoteAuthentificationService, RaplaLocale raplaLocale, Provider<RaplaBuilder> raplaBuilder,
-                 Provider<RaplaObjectActions> raplaObjectActionsProvider, RaplaResources i18n)
-    {
+                 Provider<RaplaObjectActions> raplaObjectActionsProvider, RaplaResources i18n) {
         this.clientFacade = facade;
         this.i18n = i18n;
         this.facade = clientFacade.getRaplaFacade();
@@ -50,44 +54,53 @@ public class JsApi
         this.raplaObjectActionsProvider = raplaObjectActionsProvider;
     }
 
-    public User getUser( ) throws RaplaException
-    {
+    public User getUser() throws RaplaException {
         return clientFacade.getUser();
     }
 
-    public RaplaFacade getFacade()
-    {
+    public RaplaFacade getFacade() {
         return facade;
     }
 
-    public CalendarSelectionModel getCalendarModel()
-    {
+    public CalendarSelectionModel getCalendarModel() {
         return calendarModel;
     }
 
-    public RemoteAuthentificationService getRemoteAuthentification()
-    {
+    public RemoteAuthentificationService getRemoteAuthentification() {
         return remoteAuthentificationService;
     }
 
-    public RaplaLocale getRaplaLocale()
-    {
+    public RaplaLocale getRaplaLocale() {
         return raplaLocale;
     }
 
-    public ReservationController getReservationController()
-    {
+    public ReservationController getReservationController() {
         return reservationController;
     }
 
-    public RaplaBuilder createBuilder() { return raplaBuilder.get();};
+    public RaplaBuilder createBuilder() {
+        return raplaBuilder.get();
+    }
 
-    public RaplaObjectActions createActions()
-    {
+    public RaplaObjectActions createActions() {
         return raplaObjectActionsProvider.get();
     }
 
     public RaplaResources getI18n() {
         return i18n;
     }
+
+    public CalendarOptions getCalendarOptions() throws RaplaException {
+        return RaplaComponent.getCalendarOptions(getUser(), facade);
+    }
+
+    public String[] getCalendarNames() throws RaplaException {
+        final Preferences preferences = getFacade().getPreferences(getUser());
+        Map<String, CalendarModelConfiguration> exportMap = preferences.getEntry(AutoExportPlugin.PLUGIN_ENTRY);
+        if (exportMap != null) {
+            return exportMap.keySet().toArray(new String[]{});
+        }
+        return new String[]{};
+    }
+
 }
