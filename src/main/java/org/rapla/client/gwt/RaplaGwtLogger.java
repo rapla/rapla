857570b10afccb3113dc459e68df40fa9eafd4ff
kohlhaas <christopher.kohlhaas@erdkante.de>
2018-03-09 19:20:08 +0100

    trace replaced with log

--- src/main/java/org/rapla/client/gwt/RaplaGwtLogger.java
+++ src/main/java/org/rapla/client/gwt/RaplaGwtLogger.java
@@ -28,7 +28,6 @@ var d = new Date();
 var message = d.getHours() +":"+d.getMinutes()+":" + d.getSeconds() + "." + d.getMilliseconds() +" "+ string ;
 switch ( logLevel) {
 case -1:
-    console.trace( message ); break;
 case 0:
     console.log( message ); break;
 case 1:
