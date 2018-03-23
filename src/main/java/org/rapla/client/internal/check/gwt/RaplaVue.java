0b5ed6806adf1788ffddf3e4d84e52c810c1122a
Fabian Tarrach <33595266+ftarrach@users.noreply.github.com>
2018-03-23 20:45:09 +0100

    vue-router for gwt interop

--- src/main/java/org/rapla/client/internal/check/gwt/RaplaVue.java
+++ src/main/java/org/rapla/client/internal/check/gwt/RaplaVue.java
@@ -4,14 +4,15 @@ import jsinterop.annotations.JsMethod;
 import jsinterop.annotations.JsPackage;
 import jsinterop.annotations.JsType;
 
-@JsType(name = "raplaVue", isNative = true, namespace = JsPackage.GLOBAL)
+@JsType(name = "raplaVue",
+        isNative = true,
+        namespace = JsPackage.GLOBAL)
 public class RaplaVue {
 
-	@JsMethod(name = "$emit")
-	public static native void emit(String eventname);
+  @JsMethod(name = "$emit")
+  public static native void emit(String eventname);
 
-	@JsMethod(name = "$emit")
-	public static native void emit(String eventname, Object params);
-
+  @JsMethod(name = "$emit")
+  public static native void emit(String eventname, Object params);
 
 }
