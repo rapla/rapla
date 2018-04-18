73ef95c483b28959ded5cde77e1db437e28f9634
kohlhaas <christopher.kohlhaas@erdkante.de>
2018-04-18 07:32:24 +0200

    Moved TreeFactory

---
+++ src/main/java/org/rapla/client/TreeFactory.java
@@ -0,0 +1,50 @@
+package org.rapla.client;
+
+import org.rapla.entities.Category;
+import org.rapla.entities.Named;
+import org.rapla.entities.domain.Allocatable;
+import org.rapla.entities.dynamictype.ClassificationFilter;
+import org.rapla.facade.Conflict;
+import org.rapla.framework.RaplaException;
+
+import java.util.Collection;
+
+public interface TreeFactory {
+
+	AllocatableNodes createAllocatableModel(ClassificationFilter[] filter) throws RaplaException;
+
+	RaplaTreeNode createClassifiableModel(Allocatable[] classifiables, boolean useCategorizations);
+
+	RaplaTreeNode createConflictModel(Collection<Conflict> conflicts ) throws RaplaException;
+	
+    RaplaTreeNode newNamedNode(Named element);
+
+	RaplaTreeNode createModel(Collection<Category> categories, boolean includeChildren);
+
+	RaplaTreeNode newStringNode(String s);
+
+	RaplaTreeNode newRootNode();
+
+	class AllocatableNodes
+	{
+		public final RaplaTreeNode allocatableNode;
+		public final boolean filtered;
+
+		public AllocatableNodes(RaplaTreeNode allocatableNode, boolean filtered)
+		{
+			this.allocatableNode = allocatableNode;
+			this.filtered = filtered;
+		}
+
+		public RaplaTreeNode getAllocatableNode()
+		{
+			return allocatableNode;
+		}
+
+		public boolean isFiltered()
+		{
+			return filtered;
+		}
+	}
+
+}
